239af09c7ba7f6208ac03c636aefb9622638d357
Josh Payne <paynejoshua1@gmail.com>
2021-07-19 11:50:23 -0700

    changed css file to reflect changes to dice file

--- src/components/dice/index.js
+++ src/components/dice/index.js
@@ -11,34 +11,34 @@ function DiceElement(props){
                 { props.diceResult === 1
                 
                 ? 
-                    <img src={DiceShortCut.OneDice} className="imgDice" onClick={DiceSelect} dicenumber={1} dicecolor={props.bgColor} style={{backgroundColor: props.bgColor}} />
+                    <img src={DiceShortCut.OneDice} alt="1 dice" className="dice" onClick={DiceSelect} dicenumber={1} dicecolor={props.bgColor} style={{backgroundColor: props.bgColor}} />
 
                 : props.diceResult === 2 
                 
                 ?
-                    <img src={DiceShortCut.TwoDice} className="imgDice" onClick={DiceSelect} dicenumber={2} dicecolor={props.bgColor} style={{backgroundColor: props.bgColor}} />
+                    <img src={DiceShortCut.TwoDice} alt="2 dice" className="dice" onClick={DiceSelect} dicenumber={2} dicecolor={props.bgColor} style={{backgroundColor: props.bgColor}} />
 
                 : props.diceResult === 3
 
                 ?
-                    <img src={DiceShortCut.ThreeDice} className="imgDice" onClick={DiceSelect} dicenumber={3} dicecolor={props.bgColor} style={{backgroundColor: props.bgColor}} />
+                    <img src={DiceShortCut.ThreeDice} alt="3 dice" className="dice" onClick={DiceSelect} dicenumber={3} dicecolor={props.bgColor} style={{backgroundColor: props.bgColor}} />
 
                 : props.diceResult === 4
 
                 ?
-                    <img src={DiceShortCut.FourDice} className="imgDice" onClick={DiceSelect} dicenumber={4} dicecolor={props.bgColor} style={{backgroundColor: props.bgColor}} />
+                    <img src={DiceShortCut.FourDice} alt="4 dice" className="dice" onClick={DiceSelect} dicenumber={4} dicecolor={props.bgColor} style={{backgroundColor: props.bgColor}} />
 
                 : props.diceResult === 5
 
                 ?
 
-                    <img src={DiceShortCut.FiveDice} className="imgDice" onClick={DiceSelect} dicenumber={5} dicecolor={props.bgColor} style={{backgroundColor: props.bgColor}} />
+                    <img src={DiceShortCut.FiveDice} alt="5 dice" className="dice" onClick={DiceSelect} dicenumber={5} dicecolor={props.bgColor} style={{backgroundColor: props.bgColor}} />
 
                 : props.diceResult === 6
 
                 &&
 
-                    <img src={DiceShortCut.SixDice} className="imgDice" onClick={DiceSelect} dicenumber={6} dicecolor={props.bgColor} style={{backgroundColor: props.bgColor}} />
+                    <img src={DiceShortCut.SixDice} alt="6 dice" className="dice" onClick={DiceSelect} dicenumber={6} dicecolor={props.bgColor} style={{backgroundColor: props.bgColor}} />
 
                     }
 
